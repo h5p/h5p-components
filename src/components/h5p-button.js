0464ba1b76e093b0d55a6b49d1252db8aa136db8
Vilde Voss Stabell <vilde.voss.stabell@gmail.com>
2025-03-25 12:56:27 +0100

    VA-654 Add button type

--- src/components/h5p-button.js
+++ src/components/h5p-button.js
@@ -2,27 +2,29 @@ var H5P = H5P || {};
 H5P.Components = H5P.Components || {};
 createElement = H5P.Components.utils.createElement;
 
-/**
- * Create a themed, responsive button
- * @param {HTMLElement} container
- * @param {string} [params.label] The button text
- * @param {string} [params.ariaLabel] The screenreader friendly text. Default is label.
- * @param {string} [params.tooltip] The tooltip to show on hover/focus. Default is label if icon enabled. Needed since icon only button on small screens
- * @param {string} [params.type] Which (visual) type of button it is. Options are primary (default), secondary and nav.
- * @param {string} [params.icon] Which icon to show on the button. Options are check, retry, done, show-results, book, flip, reveal-answer, next, previous
- * @param {string} [params.classes] Additional classes to add to the button
- * @param {function} [params.onClick] The function to perform once the button is clicked
- */
 H5P.Components.Button = (function () {
+  /**
+   * Create a themed, responsive button
+   * @param {HTMLElement} container
+   * @param {string} [params.label] The button text
+   * @param {string} [params.ariaLabel] The screenreader friendly text. Default is label.
+   * @param {string} [params.tooltip] The tooltip to show on hover/focus. Default is label if icon enabled. Needed since icon only button on small screens
+   * @param {string} [params.styleType] Which (visual) type of button it is. Options are primary (default), secondary and nav.
+   * @param {string} [params.icon] Which icon to show on the button. Options are check, retry, done, show-results, book, flip, reveal-answer, next, previous
+   * @param {string} [params.classes] Additional classes to add to the button
+   * @param {function} [params.onClick] The function to perform once the button is clicked
+   * @param {string} [params.buttonType] which html type the button should be. Default is button
+   */
   function Button (container, params) {
     const button = createElement('button', {
       innerHTML: params.label ? `<span class="h5p-theme-label">${params.label}</span>` : '',
       ariaLabel: params.ariaLabel ?? params.label,
       classList: params.classes ?? '',
       onclick: params.onClick,
+      type: params.buttonType ?? 'button',
     });
 
-    switch (params.type) {
+    switch (params.styleType) {
       case 'secondary':
         button.classList.add('h5p-theme-secondary-cta');
         break;
