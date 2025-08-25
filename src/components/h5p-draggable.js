9120b8894990246d5b1344a5f44d45c858c25674
William Almnes <william.almnes@gmail.com>
2025-08-25 11:02:56 +0200

    VA-1149 Add basic draggable component

---
+++ src/components/h5p-draggable.js
@@ -0,0 +1,58 @@
+// eslint-disable-next-line no-use-before-define, no-var
+var H5P = H5P || {};
+H5P.Components = H5P.Components || {};
+
+H5P.Components.Draggable = (function ($) {
+  /**
+   * @typedef DraggableParams
+   * @type {object}
+   * @property {string} label A label for the draggable element.
+   * @property {[number]} tabIndex Tabindex to use on the draggable element (default 0).
+   * @property {[boolean]} ariaGrabbed Initialize the grabbed state on the draggable (default false).
+   * @property {[boolean]} hasHandle A boolean determining if the draggable has visual handles or not.
+   * @property {function} handleRevert A callback function to handle revert.
+   * @property {function} handleDragEvent A callback function for the drag event.
+   * @property {function} handleDragStartEvent A callback function for the dragstart event.
+   * @property {function} handleDragStopEvent A callback function for the dragend event.
+   */
+
+  /**
+   * Create a themed, Draggable element
+   * @param {DraggableParams} params A set of parameters to configure the Draggable component.
+   * @returns {HTMLElement} The Draggable element.
+   */
+  function Draggable(params) {
+    const { createElement } = H5P.Components.utils;
+    // @todo VA-1146: Rename to 'h5p-draggable'. Currently using longer classname to avoid
+    // conflict with class in Drag the Words. 
+    let classes = 'h5p-component-draggable';
+
+    if (params.hasHandle) {
+      classes += ' h5p-component-draggable--has-handle';
+    }
+
+    const draggable = createElement('div', {
+      classList: classes,
+      innerHTML: `<span>${params.label}</span><span class="h5p-hidden-read"></span>`,
+      role: 'button',
+      tabIndex: params.tabIndex ?? 0,
+    });
+
+    // Have to set it like this, because it cannot be set with createElement.
+    draggable.setAttribute('aria-grabbed', params.ariaGrabbed ?? false);
+
+    // Use jQuery draggable (for now)
+    $(draggable).draggable({
+      revert: params.handleRevert,
+      drag: params.handleDragEvent,
+      start: params.handleDragStartEvent,
+      stop: params.handleDragStopEvent,
+      containment: params.containment,
+    });
+    
+
+    return draggable;
+  }
+
+  return Draggable;
+})(H5P.jQuery);
